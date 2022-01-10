052f7e98bcadbe441878a754228248d19d76a0f5
EIT-LEFLOCH-SALVADOR-TP1 <97032529+EIT-LEFLOCH-SALVADOR-TP1@users.noreply.github.com>
2022-01-10 17:14:58 +0100

    Add files via upload

---
+++ src/image.py
@@ -0,0 +1,127 @@
+from skimage import io
+from skimage.transform import resize
+import matplotlib.pyplot as plt
+import numpy as np
+
+class Image:
+    def __init__(self):
+        """Initialisation d'une image composee d'un tableau numpy 2D vide
+        (pixels) et de 2 dimensions (H = height et W = width) mises a 0
+        """
+        self.pixels = None
+        self.H = 0
+        self.W = 0
+    
+
+    def set_pixels(self, tab_pixels):
+        """ Remplissage du tableau pixels de l'image self avec un tableau 2D (tab_pixels)
+        et affectation des dimensions de l'image self avec les dimensions 
+        du tableau 2D (tab_pixels) 
+        """
+        self.pixels = tab_pixels
+        self.H, self.W = self.pixels.shape
+
+
+    def load(self, file_name):
+        """ Lecture d'un image a partir d'un fichier de nom "file_name"""
+        self.pixels = io.imread(file_name)
+        self.H,self.W = self.pixels.shape 
+        print("lecture image : " + file_name + " (" + str(self.H) + "x" + str(self.W) + ")")
+
+
+    def display(self, window_name):
+        """Affichage a l'ecran d'une image"""
+        fig = plt.figure(window_name)
+        if (not (self.pixels is None)):
+            io.imshow(self.pixels)
+            io.show()
+        else:
+            print("L'image est vide. Rien à afficher")
+
+
+    #==============================================================================
+    # Methode de binarisation
+    # 2 parametres :
+    #   self : l'image a binariser
+    #   S : le seuil de binarisation
+    #   on retourne une nouvelle image binarisee
+    #==============================================================================
+    def binarisation(self, S):
+		# creation d'une image vide
+        im_bin = Image()
+        
+        # affectation a l'image im_bin d'un tableau de pixels de meme taille
+        # que self dont les intensites, de type uint8 (8bits non signes),
+        # sont mises a 0
+        im_bin.set_pixels(np.zeros((self.H, self.W), dtype=np.uint8))
+        for l in range(self.H):
+            for c in range(self.W):
+                if self.pixels[l][c] >= S:
+                    im_bin.pixels[l][c] = 255
+                else :
+                    im_bin.pixels[l][c] = 0
+                
+
+        # TODO: boucle imbriquees pour parcourir tous les pixels de l'image im_bin
+        # et calculer l'image binaire
+        
+        
+        return im_bin    
+
+
+    #==============================================================================
+    # Dans une image binaire contenant une forme noire sur un fond blanc
+    # la methode 'localisation' permet de limiter l'image au rectangle englobant
+    # la forme noire
+    # 1 parametre :
+    #   self : l'image binaire que l'on veut recadrer
+    #   on retourne une nouvelle image recadree
+    #==============================================================================
+    def localisation(self):
+        im_localis = Image()
+        l_min=self.H
+        c_min=self.W
+        l_max=0
+        c_max=0
+        for l in range(self.H):
+            for c in range(self.W):
+                if self.pixels[l][c] == 0:
+                    if l < l_min:
+                        l_min = l
+                    if c < c_min:
+                        c_min = c
+                    if l > l_max:
+                        l_max = l
+                    if c > c_max :
+                        c_max = c
+        im_localis.pixels = self.pixels[l_min:l_max+1, c_min:c_max+1]
+        im_localis.H = l_max - l_min
+        im_localis.W = c_max - c_min
+        return (im_localis)
+
+    #==============================================================================
+    # Methode de redimensionnement d'image
+    #==============================================================================
+    def resize(self, new_H, new_W):
+        im_resized = Image()
+        im_resized.pixels = resize(self.pixels, (new_H,new_W), 0)
+        im_resized.pixels = np.uint8(im_resized.pixels*255)
+        im_resized.H = new_H
+        im_resized.W = new_W
+        return (im_resized)
+        
+
+
+    #==============================================================================
+    # Methode de mesure de similitude entre l'image self et un modele im
+    #==============================================================================
+    def similitude(self, im):
+        simi=0
+        for l in range (self.H):
+            for c in range (self.W):
+                if self.pixels[l,c] == im.pixels[l,c]:
+                    simi += 1
+        proportion = float(simi/(self.H*self.W))
+        return proportion
+    
+
---
+++ src/reconnaissance.py
@@ -0,0 +1,30 @@
+from image import Image
+
+def lecture_modeles(chemin_dossier):
+    fichiers= ['_0.png','_1.png','_2.png','_3.png','_4.png','_5.png','_6.png', 
+            '_7.png','_8.png','_9.png']
+    liste_modeles = []
+    for fichier in fichiers:
+        model = Image()
+        model.load(chemin_dossier + fichier)
+        liste_modeles.append(model)
+    return liste_modeles
+
+
+def reconnaissance_chiffre(image, liste_modeles, S):
+    liste=[]
+    bin=image.binarisation(S)
+    loc=bin.localisation()
+    
+    for ref in liste_modeles:
+        im_resized=loc.resize(ref.H,ref.W)
+        s = im_resized.similitude(ref)
+        s1 = round(s,3)
+        liste.append(s1)
+        for i in range (len(liste)):
+                num=0
+                if liste [i]>liste[num]:
+                    num=i
+    
+    return liste.index(max(liste))
+
